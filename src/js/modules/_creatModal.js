47d5f55ca0a6e0ea8437578fb566c5805d041555
Артём Ситников <sitnikov.artem91@gmail.com>
2023-04-12 23:13:54 +0300

    add     changeForm(elem,obj){
    clear

--- src/js/modules/_creatModal.js
+++ src/js/modules/_creatModal.js
@@ -5,14 +5,15 @@ class Modal {
     this.cardiologist= cardiologist;
     this.dentist = dentist;
     this.therapeutic =therapeutic
-}
+    }
     createModal(){
         let modal = `
     <section class="modal">
-    <div class="creating__visit">
-        <div class="modal-login__close-btn">&times;</div>
-              <h3 class="modal-login__title">Створити Візит</h3>  
-              <select name="doctor"  class="choose__doctor">
+    <div class="modal__content">
+        <div class="modal__content__close-btn">&times;</div>
+              <h3 class="modal__content__title">Створити Візит</h3>  
+              <select name="doctor" class="modal__content__choose__doctor">
+                <option name="doctor" >Усі</option>
                 <option name="doctor" value="Сardiologist">${this.cardiologist}</option>
                 <option name="doctor" value="Dentist">${this.dentist}</option>
                 <option name="doctor" value="Therapeutic">${this.therapeutic}</option>   
@@ -30,7 +31,7 @@ class Modal {
         document.querySelector(".modal").remove()
     }
      clickCloseModal(modal,closeButton) {
-        modal.addEventListener('click', e => {
+        modal.addEventListener('click', (e) => {
             if (e.target === modal || e.target ===closeButton) {
                 this.closeModal()
             }
@@ -39,15 +40,15 @@ class Modal {
     createModalEnter(title){
         let modal = `
         <section class="modal">
-        <div class="content">
-        <h2>${title}</h2>
-        <div class="modal-login__close-btn">&times;</div>
-            <label for='email'>Email</label>
+        <div class="modal__content">
+        <h2 class="modal__content__title">${title}</h2>
+        <div class="modal__content__close-btn">&times;</div>
+            <label for='email'>Email:</label>
             <input type='email' name='email' class="modal__email"/>
-            <label for='password'>Password</label>
+            <label for='password'>Password:</label>
             <input type='password' name='password' class="modal__password"/>
-        </div>
-        <button class="send-data">Send</button>
+            <button class="modal__content__registration-btn btn btn--red">Увійти</button>
+            </div>
         </section>
          `
         document.body.insertAdjacentHTML("beforeend",modal)
@@ -77,7 +78,7 @@ class Modal {
   })
         })
         }
-putDataFromForma(btn,modal,tokenUser,cardId){
+    putDataFromForma(btn,modal,tokenUser,cardId){
             btn.addEventListener('click',()=>{
                 const user ={};
                 let all = modal.querySelectorAll('select,input,textarea');
@@ -96,8 +97,7 @@ putDataFromForma(btn,modal,tokenUser,cardId){
       .then(response => response.json())
             })
             }
-
-}
+    }
 
 class Visit {
     constructor(fullName,purpose,description,impotent,status){
@@ -111,18 +111,18 @@ createForma(elem){
     let addForm= `
 
                 <label for="fullName"class="visit__labels">${this.fullName}:</label>
-                     <input name="fullName" type="text" value="" class="border">
+                     <input name="fullName" type="text" value="" class="border input--size">
                <label for="visitType" class="visit__labels">${this.purpose}:</label>
-                     <input name="visitType" type="text" value="" class="border">
+                     <input name="visitType" type="text" value="" class="border input--size">
                 <label for="visitDescription" class="visit__labels">${this.description}:</label>
-                     <textarea cols="10" id="" name="visitDescription" value="" rows="5"></textarea>
+                     <textarea cols="10" class="border"  name="visitDescription" value="" rows="5"></textarea>
                 <label for="impotent" class="visit__labels">${this.status}:</label>
-                <select name="visitStatus">
+                <select name="visitStatus" class="input--size">
                     <option name="visitStatus" value="Вікрита">Вікрита</option>
                     <option name="visitStatus"value="Закрита">Закрита</option>
                 </select>
                 <label for="impotent" class="visit__labels">${this.impotent}:</label>
-                <select name="visitUrgency">
+                <select name="visitUrgency" class="input--size">
                     <option  value="Невідкладна">Невідкладна</option>
                     <option value="Пріоритетна">Пріоритетна</option>
                     <option value="Звичайна">Звичайна</option>                
@@ -134,25 +134,24 @@ changeForm(elem,obj){
     let changeForm= `
 
                 <label for="fullName"class="visit__labels">${this.fullName}:</label>
-                     <input name="fullName" type="text" value="${pationtName}" class="border">
+                     <input name="fullName" type="text" value="${pationtName}" class="border input--size">
                <label for="visitType" class="visit__labels">${this.purpose}:</label>
-                     <input name="visitType" type="text" value="${typeVisit}" class="border">
+                     <input name="visitType" type="text" value="${typeVisit}" class="border input--size">
                 <label for="visitDescription" class="visit__labels">${this.description}:</label>
-                     <textarea cols="10" id="" name="visitDescription" rows="5">${description}</textarea>
+                     <textarea cols="10" class="input--size" name="visitDescription" rows="5">${description}</textarea>
                 <label for="impotent" class="visit__labels">${this.status}:</label>
-                <select name="visitStatus">
+                <select name="visitStatus" class="input--size">
                     <option name="visitStatus" value="Вікрита">Вікрита</option>
                     <option name="visitStatus" value="Закрита">Закрита</option>
                 </select>
                 <label for="impotent" class="visit__labels">${this.impotent}:</label>
-                <select name="visitUrgency">
+                <select name="visitUrgency" class="input--size">
                     <option value="Невідкладна">Невідкладна</option>
                     <option value="Пріоритетна">Пріоритетна</option>
                     <option value="Звичайна">Звичайна</option>                
               </select>`
             elem.insertAdjacentHTML("beforeend",changeForm)
 }
-c
 
 }
 class VisitСardiologist extends Visit{
@@ -168,16 +167,31 @@ class VisitСardiologist extends Visit{
         let addForm =`
         <div class="add__inputs Сardiologist">
            <label class="visit__labels">${this.pressure}:</label>
-                <input name="bloodPressure" type="text" class="border">
+                <input name="bloodPressure" type="text" class="border input--size">
            <label class="visit__labels">${this.indexWeight}:</label>
-                <input name="bodyMassIndex" type="text" class="border">
+                <input name="bodyMassIndex" type="text" class="border input--size">
            <label class="visit__labels">${this.heartDisease}:</label>
-                <input name="cardiovascularDisease" type="text" class="border">
+                <input name="cardiovascularDisease" type="text" class="border input--size">
            <label for="age"class="visit__labels">${this.age}:</label>
-                <input name="age"type="text" class="border">
+                <input name="age"type="text" class="border input--size">
        </div>
         `
         elem.insertAdjacentHTML("beforeend", addForm)
+    }
+    changeForm(elem,obj){
+        super.changeForm(elem,obj)
+
+        let changeForm =`
+           <<label class="visit__labels">${this.pressure}:</label>
+           <input name="bloodPressure" value=${bloodPressure} type="text" class="border input--size">
+      <label class="visit__labels">${this.indexWeight}:</label>
+           <input name="bodyMassIndex" value=${bodyMassIndex} type="text" class="border input--size">
+      <label class="visit__labels">${this.heartDisease}:</label>
+           <input name="cardiovascularDisease" value=${cardiovascularDisease} type="text" class="border input--size">
+      <label for="age"class="visit__labels">${this.age}:</label>
+           <input name="age"type="text" value=${age}class="border input--size">
+        `
+        elem.insertAdjacentHTML("beforeend", changeForm)
     }
 }
 class VisitDentist extends Visit{
@@ -190,16 +204,17 @@ class VisitDentist extends Visit{
         let addForm =`
         <div class="add__inputs Dentist">
            <label for="visitDate" class="visit__labels">${this.date}:</label>
-           <input type="date" name="visitDate" class="border">
+           <input type="date" name="visitDate" class="border input--size">
        </div>
         `
         elem.insertAdjacentHTML("beforeend", addForm)
     }
     changeForm(elem,obj){
         super.changeForm(elem,obj)
+        const {visitDate:visitDateValue}=obj;
         let changeForm =`
            <label for="visitDate" class="visit__labels">${this.date}:</label>
-           <input type="date" name="visitDate" class="border">
+           <input type="date" name="visitDate" value=${visitDateValue} class="border input--size">
         `
         elem.insertAdjacentHTML("beforeend", changeForm)
     }
@@ -214,7 +229,7 @@ class VisitTherapeutic extends Visit{
         let addForm =`
         <div class="add__inputs Therapeutic">
            <label for="age" class="visit__labels">${this.age}:</label>
-           <input type="text" name="age" class="border">
+           <input type="text" name="age" class="border input--size">
        </div>
         `
         elem.insertAdjacentHTML("beforeend", addForm)
