3cae5ab138d9f9b3991ef470e598a663e79c605f
Артём Ситников <sitnikov.artem91@gmail.com>
2023-04-12 09:33:06 +0300

    delete method change from renderingLoginCards.Add listener on block board-of-cards. Made function async function createModalForChanges(tokenUser,parentElementOfClickId){

--- src/js/modules/_localStorage.js
+++ src/js/modules/_localStorage.js
@@ -1,95 +1,113 @@
 // /: d27dc183-2d35-4901-9c3c-7ccd9e890e13 sitnikov.artem91@gmail.com pass:12345
-// import {token} from "./_login.js"
-// import {selectCondition ,selectVisitsTerm,inputSearch,btnSearch,arrayOfCards,Validation} from './_cardsFilters.js'
-// import {Card,CardiologistCard,DentistCard,TherapistCard,boardOfCards} from './_creatCard.js'
-// // import {toggleClass} from "../app.js"
+import { tokenUser} from "./_login.js"
+import {selectCondition ,selectVisitsTerm,inputSearch,btnSearch,arrayOfCards,Validation} from './_cardsFilters.js'
+import {Card,CardiologistCard,DentistCard,TherapistCard,boardOfCards} from './_creatCard.js'
+import {Modal,Visit,VisitСardiologist,VisitDentist,VisitTherapeutic}from './_creatModal.js'
+import {visitDentist} from './_createVisit.js'
+async function renderingLoginCards(tokenUser){
+  let response = await fetch("https://ajax.test-danit.com/api/v2/cards", {
+    method: 'GET',
+    headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${tokenUser}`
+  }})
+let json = await response.json();
+let validation = new Validation(inputSearch,json,btnSearch,arrayOfCards);
+validation.validationFilters(selectCondition,"gggg");
+validation.validationFilters(selectVisitsTerm,"visitUrgency");
+validation.clickOnButtonSearch();
+let cards =new Card();
+cards.deleteCard(boardOfCards)
+// boardOfCards.addEventListener('click', (e)=>{
+//   let parentElementOfClickId = e.target.closest("div").parentElement.dataset.id;
+//   if(e.target.className === 'board-of-cards__patient-card__edit-btn' && e.target.closest("div").parentElement.dataset.id === parentElementOfClickId){
+//     createModalForChanges(tokenUser,parentElementOfClickId)
+//   }
+// })
 
-// async function renderingLoginCards(){
-// let response = await fetch("https://ajax.test-danit.com/api/v2/cards", {
-//     method: 'GET',
-//     headers: {
-//     'Content-Type': 'application/json',
-//     'Authorization': 'Bearer d27dc183-2d35-4901-9c3c-7ccd9e890e13 '
-//   }})
-// let json = await response.json()
-// let validation = new Validation(inputSearch,json,btnSearch,arrayOfCards);
-// validation.validationFilters(selectCondition,"gggg");
-// validation.validationFilters(selectVisitsTerm,"visitUrgency");
-// validation.clickOnButtonSearch()
-// let cards =new Card();
-// cards.deleteCard(boardOfCards)
-// cards.change()
-// json.forEach(card=>{
-//   console.log(card);
-// //   fetch(`https://ajax.test-danit.com/api/v2/cards/160767`, {
-// //     method: 'DELETE',
-// //     headers: {
-// //         'Authorization': "Bearer d27dc183-2d35-4901-9c3c-7ccd9e890e13"
-// //     },
-// // })
-//   const cardsValues = Object.values(card);
-//   const [doctor,...rest]= cardsValues;
-//   let objDoctor;
-//   switch (doctor) {
-//     case "Кардіолог":
-//       objDoctor = new CardiologistCard(...cardsValues);
-     
-//     break;
-//     case "Стоматолог":
-//       objDoctor =new DentistCard(...cardsValues);
+// fetch(`https://ajax.test-danit.com/api/v2/cards/161521`, {
+//   method: 'DELETE',
+//   headers: {
+//       'Authorization': `Bearer ${tokenUser}`
+//   },
+// })
+json.forEach(card=>{
+
+  const cardsValues = Object.values(card);
+  const [doctor,...rest]= cardsValues;
+  let objDoctor;
+  switch (doctor) {
+    case "Сardiologist":
+      objDoctor = new CardiologistCard(...cardsValues);
+    break;
+    case "Dentist":
+      objDoctor =new DentistCard(...cardsValues);
       
-//     break;
-//     case "Терапевт":
-//       objDoctor =new TherapistCard(...cardsValues);
+    break;
+    case "Therapeutic":
+      objDoctor =new TherapistCard(...cardsValues);
       
-//     break;
-//     default: console.log("Помилка");
-//       break;
-// }
-// objDoctor.renderingCard()
-// // objDoctor.change()
+    break;
+    default: console.log("Помилка");
+      break;
+}
+objDoctor.renderingCard()
 
-// })  
-// }
+}) 
+}
 
+document.querySelector('.board-of-cards').addEventListener('click', (e)=>{
+  let parentElementOfClickId = e.target.closest("div").parentElement.dataset.id;
+  if(e.target.className === 'board-of-cards__patient-card__edit-btn' && e.target.closest("div").parentElement.dataset.id === parentElementOfClickId){
+    createModalForChanges(tokenUser,parentElementOfClickId)
+  }
+})
 
-
-// // function setDataInLocalStorage(token){
-// //   let emailValue = document.querySelector('.modal__email').value
-// //   let passwordValue =document.querySelector('.modal__password').value
-
-// //   const user ={
-// //       email:`${emailValue}`,
-// //       password:`${passwordValue}`,
-// //       token:`${token}`
-// //   }
-
-// // localStorage.setItem("mainUser",JSON.stringify(user))
-// // }
-
-// // function validateEnter(localObj,emailInput,passwordInput,tokens){
-// // let values = JSON.parse(localObj)
-// // if(values.email === emailInput && values.password === passwordInput && values.token === tokens){
-// //   renderingLoginCards()
-// // }
-// // }
-
-// // function reloadPage(localObj){
-// //   if(typeof localObj == "undefined"){
-// //     console.error("No values")
-// //   }
-// //   else{
-// //     let values = JSON.parse(localObj);
-// //     if(Object.keys(values).includes("token")){
-// //       toggleClass('header__btn')
-// //       toggleClass('board-of-cards__text')
-// //       renderingLoginCards()
-// //     }
-// //   }
-// // }
-// // window.onload =()=>{
-// //   reloadPage(localStorage.getItem("mainUser"))
-// // }
+  
  
+async function createModalForChanges(tokenUser,parentElementOfClickId){
+  
+        let response = await fetch(`https://ajax.test-danit.com/api/v2/cards/${parentElementOfClickId}`, {
+          method: 'GET',
+          headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${tokenUser}`
+          }})
+          if(response.ok){
+          let changingCard = await response.json()
+          const {doctor}=changingCard 
+          console.log(changingCard)
+          let changeModal =new Modal("Кардіолог","Стоматолог","Терапевт");
+          
+          changeModal.createModal();
 
+          let sectionModal = document.querySelector('.modal');
+          let closeButton = document.querySelector('.modal-login__close-btn');
+          let form = document.querySelector('.modal-creating__form');
+          let btnSubmitData =document.querySelector('.btn__send__visit')
+          let optionDoctorSelected = document.querySelector('.choose__doctor').querySelectorAll('option');
+
+      
+          doctor === "Сardiologist"?optionDoctorSelected[0].selected= 'selected':""
+         
+          doctor === "Dentist"?(
+            optionDoctorSelected[1].selected ='selected',
+            visitDentist.changeForm(form,changingCard),
+            changeModal.putDataFromForma(btnSubmitData,sectionModal,tokenUser,parentElementOfClickId),            
+            changeModal.clickCloseModal(sectionModal,closeButton)
+            ):""
+          doctor === "Therapeutic"? optionDoctorSelected[2].selected= 'selected':""
+          
+          boardOfCards.innerHTML ="";
+           
+          
+          btnSubmitData.addEventListener('click', ()=>{
+            changeModal.closeModal();
+            renderingLoginCards(tokenUser)
+          }
+            )
+        }     
+      }
+
+export{renderingLoginCards,createModalForChanges}
 // export {renderingLoginCards,setDataInLocalStorage,validateEnter,reloadPage}
