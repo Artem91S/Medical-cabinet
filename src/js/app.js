a878cdb74fc655257f9694b19cb46ed4d38a673b
Артём Ситников <sitnikov.artem91@gmail.com>
2023-04-16 00:12:13 +0300

    add import exitVisit

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,14 @@
 // import './modules/_modal.js'
 // import './modules/_login.js'
 // import './modules/_createCard.js'
+import './modules/_login.js'
+import './modules/_localStorage.js'
 import './modules/_creatCard.js'
 import './modules/_cardsFilters.js'
-import './modules/_localStorage.js'
-import './modules/_login.js'
+
+
 import './modules/_createVisit.js'
+import './modules/_exitVisit.js'
 import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
