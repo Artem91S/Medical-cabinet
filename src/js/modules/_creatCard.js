10b3ac9f869cb518086d7cb1d6b18ba7af80a40a
Артём Ситников <sitnikov.artem91@gmail.com>
2023-04-15 16:10:05 +0300

    change structure of renderingCard.Change listener in method hiddenDetails

--- src/js/modules/_creatCard.js
+++ src/js/modules/_creatCard.js
@@ -1,6 +1,4 @@
-import { Modal } from "./_creatModal.js";
 import {tokenUser} from "./_login.js"
-// import {createModalForChanges} from './_localStorage.js'
 const boardOfCards =document.querySelector('.board-of-cards');
 export class Card{
     constructor(fullName,visitType,visitDescription,visitStatus,visitUrgency,id){
@@ -16,18 +14,20 @@ export class Card{
         <div class="board-of-cards__patient-card" data-doctor=${this.doctor} data-id=${this.id}>
             <h2  class="board-of-cards__patient-card__title" >Картка візиту</h2>
             <div  class="board-of-cards__patient-card__options" >
-            <p class="board-of-cards__patient-card__close-btn">&times;</p>
-            <p class="board-of-cards__patient-card__edit-btn" >&#10000;</p>
+              <p class="board-of-cards__patient-card__close-btn">&times;</p>
+              <p class="board-of-cards__patient-card__edit-btn" >&#10000;</p>
             </div>
             <div class="board-of-cards__patient-card__content">
-              <p class="board-of-cards__patient-card__property-name">Статус:</p>
-              <p class="board-of-cards__patient-card__property-name" data-name="visitStatus">${this.status}</p>
+              <p class="board-of-cards__patient-card__property-name">Доктор:</p>
+              <p class="board-of-cards__patient-card__property-name"data-name="doctor">${this.doctor}</p>
               <p class="board-of-cards__patient-card__property-name">ПІБ:</p>
               <p class="board-of-cards__patient-card__property-name" data-name="fullName">${this.fullName}</p>
+            <button class="board-of-cards__patient-card__btn-details" data-btn="${this.id}">Детальніше</button>
+            <div class="board-of-cards__patient-card__more-details" data-detailsid="${this.id}">
+              <p class="board-of-cards__patient-card__property-name">Статус:</p>
+              <p class="board-of-cards__patient-card__property-name" data-name="visitStatus">${this.status}</p>
               <p class="board-of-cards__patient-card__property-name">Терміновість візиту:</p>
               <p class="board-of-cards__patient-card__property-name" data-name="visitUrgency">${this.visitUrgency}</p>
-            <button class="board-of-cards__patient-card__btn-details" data-id="${this.id}__btn">Детальніше</button>
-            <div class="board-of-cards__patient-card__more-details">
               <p class="board-of-cards__patient-card__property-name">Тип візиту:</p>
               <p class="board-of-cards__patient-card__property-name" data-name="visitType">${this.visitType}</p>
               <p class="board-of-cards__patient-card__property-name">Опис:</p>
@@ -36,60 +36,75 @@ export class Card{
           </div>
         </div>
         `
-       document.querySelector('.board-of-cards').insertAdjacentHTML('beforeend', card);
+        boardOfCards.insertAdjacentHTML('beforeend', card);
  
       }
-  hiddenDetails(btn){
-      const card = document.querySelector('[data-id="163379"]');
-      btn.addEventListener('click', e=>{
+    hiddenDetails(btn){
+      let all = document.querySelectorAll('.board-of-cards__patient-card__more-details');
+      try{
+        window.addEventListener('click', e=>{
+          let showElement =e.target.closest("div").querySelector(".board-of-cards__patient-card__more-details");
+          if(e.target.dataset.btn === btn.dataset.btn ){
+            showElement.classList.toggle("click-on-details")
+          }
+          else{
+          all.forEach(section=>{
+          if(section.dataset.detailsid === btn.dataset.btn ){
+            section.classList.remove("click-on-details")
+          }
+          })
+          }
+        })
+      }
+      catch(err){
+        console.log(err);
+      }
       
-      e.target.closest("div").querySelector(".board-of-cards__patient-card__more-details").classList.toggle("click-on-details")
-    })
-   }
 
-deleteCard(boardOfCards){
-       boardOfCards.addEventListener("click", (e)=>{
+    // window.addEventListener("click", (e)=>{
+    //   if(e.target.parentElement.dataset.id == e.target.dataset.id){
+    //     e.target.closest("div").querySelector(".board-of-cards__patient-card__more-details").classList.toggle("click-on-details")
+
+    //   }
+    // })
+   }
+    deleteCard(blockOfCards){
+      blockOfCards.addEventListener("click", (e)=>{
         let parentElementOfClick = e.target.closest("div").parentElement;
-    if(e.target.className === "board-of-cards__patient-card__close-btn"){
-           // let cardId = parentElementOfClick.dataset.id;
-       
-          try {
-            
+          if(e.target.className === "board-of-cards__patient-card__close-btn"){
+          //  let cardId = parentElementOfClick.dataset.id;
+       try {
             fetch(`https://ajax.test-danit.com/api/v2/cards/${cardId}`, {
                 method: 'DELETE',
                 headers: {
-                    'Authorization': `Bearer ${tokenUser}`
+                    'Authorization':`Bearer ${tokenUser}`
                 },
             })
-                .then(response => {
+            .then(response => {
                     if(response.status === 200){
                       if(confirm("Ви дійсно хочити видалити картку?")) return parentElementOfClick.remove()
                     }})
         } catch (error) {
             console.error(error)
-        }
-        
+        } 
     }
   })
-       
-    }
+ }
   }
 
 export class CardiologistCard extends Card{
-constructor(doctor,fullName,visitType,visitDescription,visitStatus,visitUrgency,bloodPressure,bodyMassIndex,cardiovascularDisease,age,id){
-super(fullName,visitType,visitDescription,visitStatus,visitUrgency,id)
-this.doctor = doctor,
-this.bloodPressure = bloodPressure,
-this.bodyMassIndex = bodyMassIndex,
-this.cardiovascularDisease = cardiovascularDisease,
-this.age = age
+    constructor(doctor,fullName,visitType,visitDescription,visitStatus,visitUrgency,bloodPressure,bodyMassIndex,cardiovascularDisease,age,id){
+      super(fullName,visitType,visitDescription,visitStatus,visitUrgency,id)
+      this.doctor = doctor,
+      this.bloodPressure = bloodPressure,
+      this.bodyMassIndex = bodyMassIndex,
+      this.cardiovascularDisease = cardiovascularDisease,
+      this.age = age
 }
-renderingCard(){
-  super.renderingCard()
-  let btn = document.querySelector(`[data-id="${this.id}__btn"]`)
-  let cardContent = `
-      <p class="board-of-cards__patient-card__property-name">Доктор:</p>
-      <p class="board-of-cards__patient-card__property-name" data-name="doctor">${this.doctor}</p>
+    renderingCard(){
+      super.renderingCard()
+      let btn = document.querySelector(`[data-btn="${this.id}"]`)
+    let cardContent = `
       <p class="board-of-cards__patient-card__property-name">Вік:</p>
       <p class="board-of-cards__patient-card__property-name" data-name="age">${this.age}</p>
       <p class="board-of-cards__patient-card__property-name">Тиск:</p>
@@ -101,7 +116,7 @@ renderingCard(){
  
   `
 btn.closest("div").querySelector('.board-of-cards__patient-card__more-details').insertAdjacentHTML('beforeend',cardContent)
-super.hiddenDetails(btn)
+// super.hiddenDetails(btn)
 }
 }
 export class DentistCard extends Card{
@@ -112,15 +127,13 @@ export class DentistCard extends Card{
   }
   renderingCard(){
     super.renderingCard()
-    let btn = document.querySelector(`[data-id="${this.id}__btn"]`)
+    let btn = document.querySelector(`[data-btn="${this.id}"]`)
     let cardContent = `
-        <p class="board-of-cards__patient-card__property-name" >Доктор:</p>
-        <p class="board-of-cards__patient-card__property-name" data-name="doctor">${this.doctor}</p>
         <p class="board-of-cards__patient-card__property-name">Дата візиту:</p>
         <p class="board-of-cards__patient-card__property-name" data-name="visitDate">${this.visitDate}</p>
     `
     btn.closest("div").querySelector('.board-of-cards__patient-card__more-details').insertAdjacentHTML('beforeend',cardContent)
-  super.hiddenDetails(btn)
+    super.hiddenDetails(btn)
 
   }
   }
@@ -132,15 +145,14 @@ export class TherapistCard extends Card{
     }
     renderingCard(){
       super.renderingCard()
-      let btn = document.querySelector(`[data-id="${this.id}__btn"]`);
+      let btn = document.querySelector(`[data-btn="${this.id}"]`)
+
       let cardContent = `
-          <p class="board-of-cards__patient-card__property-name">Доктор:</p>
-          <p class="board-of-cards__patient-card__property-name"data-name="doctor">${this.doctor}</p>
           <p class="board-of-cards__patient-card__property-name">Вік:</p>
           <p class="board-of-cards__patient-card__property-name"data-name="age">${this.ageTherapist}</p>
       `
       btn.closest("div").querySelector('.board-of-cards__patient-card__more-details').insertAdjacentHTML('beforeend',cardContent)
-    super.hiddenDetails(btn)
+      super.hiddenDetails(btn)
     }
     }
 export {boardOfCards}
