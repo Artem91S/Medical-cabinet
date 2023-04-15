3d9d392631a921799289677621e1b95d135cbfae
Артём Ситников <sitnikov.artem91@gmail.com>
2023-04-15 16:07:47 +0300

    add filterArrayOfCards and flag isOpen to fix problem with working modal metod validationFilters

--- src/js/modules/_cardsFilters.js
+++ src/js/modules/_cardsFilters.js
@@ -2,32 +2,38 @@ const selectCondition =document.querySelector('.cards-filters__dropdown-conditio
 const selectVisitsTerm =document.querySelector('.cards-filters__dropdown-visits-term');
 const inputSearch =document.querySelector('.cards-filters__input-search');
 const btnSearch = document.querySelector('.cards-filters__btn-search');
- let arrayOfCards = [];
- export  class Validation{
-    constructor(searchInput,arrayCards,btn,array){
+
+ export class Validation{
+    constructor(searchInput,array,btn){
         this.searchInput = searchInput;
-        this.arrayCards = arrayCards;
+        this.filterCards = [];
         this.array =array;
         this.btn = btn
     }
-    validationFilters(select,key){
+    validationFilters(select,key,isOpen){
+        
         this.btn.addEventListener("click",()=>{
-            this.array = this.arrayCards.filter(userCard =>{
+            const filterArrayOfCards =isOpen?this.filterCards:this.array 
+            this.filterCards = filterArrayOfCards.filter(userCard =>{
+                
                 if(select.value === "Усі"){
                     let allCards =document.querySelectorAll('.board-of-cards__patient-card');
                     allCards.forEach(card=>{
-                        card.classList.remove("board-of-cards__patient-card__hidden-filter-card")})
-                    return this.array
+                        if(card.dataset.id !==userCard.id)
+                        card.classList.add("board-of-cards__patient-card__hidden-filter-card")})
+
+                    return this.filterCards
                 }
+              
                 let changeClass = userCard[key]!== select.value ?"add":"remove"
                 document.querySelector(`[data-id="${userCard.id}"]`).classList[changeClass]("board-of-cards__patient-card__hidden-filter-card")        
                 return changeClass == "remove"
                      })
-        })
+                    })
     }
     clickOnButtonSearch(){
         this.btn.addEventListener("click",()=>{
-            this.array.forEach(card=>{
+            this.filterCards.forEach(card=>{
                 document.querySelector(`[data-id="${card.id}"]`).classList.remove("board-of-cards__patient-card__hidden-filter-card")
                  const isInStr =(element)=>element.toString().toLowerCase().match(this.searchInput.value.toLowerCase());
                  if(!Object.values(card).some(isInStr)) return document.querySelector(`[data-id="${card.id}"]`).classList.add("board-of-cards__patient-card__hidden-filter-card")
@@ -36,6 +42,5 @@ const btnSearch = document.querySelector('.cards-filters__btn-search');
     }
   
  }
-
-export {selectCondition ,selectVisitsTerm,inputSearch,btnSearch,arrayOfCards}
+export {selectCondition ,selectVisitsTerm,inputSearch,btnSearch}
 
