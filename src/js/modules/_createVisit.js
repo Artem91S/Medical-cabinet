94d578b4b13d7e7016eb3f57c807f3d777caa074
Артём Ситников <sitnikov.artem91@gmail.com>
2023-04-16 14:25:26 +0300

    delete comments

--- src/js/modules/_createVisit.js
+++ src/js/modules/_createVisit.js
@@ -1,6 +1,6 @@
 import {tokenUser} from './_login.js'
 import {Modal,VisitСardiologist,VisitDentist,VisitTherapeutic} from './_creatModal.js'
-import {getDataFromForma} from './_localStorage.js'
+import {getDataFromForma} from './_functionsCards.js'
 
 let modal = new Modal("Кардіолог","Стоматолог","Терапевт");
 let visitСardiologist = new VisitСardiologist("ПІБ","Мета візиту","Короткий опис візиту","Tерміновість","Звичайний тиск","Індекс маси тіла","Перенесені захворювання серцево-судинної системи","Вік","Статус візиту")
@@ -23,7 +23,7 @@ createVisitButton.addEventListener("click",(e)=>{
     makeDoctorsObject("Стоматолог",visitDentist.createFormDentist,visitDentist)
     makeDoctorsObject("Терапевт",visitTherapeutic.createFormTherapeutic,visitTherapeutic)
     chooseDoctor.addEventListener("change", ()=>{
-        form.innerHTML="";////delete form content
+        form.innerHTML="";
         doctors[chooseDoctor.value](form);
         getDataFromForma(tokenUser,modal)
        
