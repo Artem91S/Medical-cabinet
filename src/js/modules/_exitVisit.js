4aada6ff543af8ea0bdf6ed62d7a82da859895b4
Артём Ситников <sitnikov.artem91@gmail.com>
2023-04-16 13:53:09 +0300

    delete toggleClass function and add removing class for boaderText

--- src/js/modules/_exitVisit.js
+++ src/js/modules/_exitVisit.js
@@ -1,16 +1,12 @@
 import {toggleClass} from '../app.js'
 const BtnExit =document.querySelector('.header__btn-exit');
-
 function exitFromVisit(){
     BtnExit.addEventListener('click',()=>{
-    
-        toggleClass('board-of-cards__text')
+        document.querySelector('.board-of-cards__text').classList.remove('hidden')
         toggleClass('header__btn')
         toggleClass('cards-filters')
         localStorage.removeItem("token");
-        document.querySelectorAll('.board-of-cards__patient-card').forEach(card=>card.remove())
-      
-        
+        document.querySelectorAll('.board-of-cards__patient-card').forEach(card=>card.remove())  
     })
 }
 exitFromVisit()
